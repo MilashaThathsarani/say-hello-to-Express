b377d3f1689b2cf9ab44936744a28a01af32c36f
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-08-22 00:41:04 +0530

    First Commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = 4000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     console.log('Get Request has come')
     res.send('Hello World!')
@@ -10,7 +12,18 @@ app.get('/', (req, res) => {
 
 app.get('/customer', (req, res) => {
     // console.log('Get Request has come')
+    console.log(req.body)
     res.send('Hello Customer!')
+})
+
+app.get('/customer/:id', (req, res) => {
+    // console.log('Get Request has come')
+    console.log(req.keyParams);
+})
+
+app.get('/customer', (req, res) => {
+    // console.log('Get Request has come')
+    console.log(req.query);
 })
 
  app.listen(port,(req,res) => {

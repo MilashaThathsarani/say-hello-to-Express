915cf46884b25b1b475af5ff7a68e5e2d2b59660
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-09-10 13:19:02 +0530

    Updated files

--- app.js
+++ app.js
@@ -1,63 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose')
-//const customer = require('./routes/customer.js')
-//const item = require('./routes/item')
-//const user = require('./routes/user')
+const express = require('express')
+const mongoose =require('mongoose')
 
-const app = express();
-const port = 4000;
+/*const customer=require('./routes/customer')*/
+// const item=require('./routes/item')
+// const user=require('./routes/user')
+const app = express()
+const port = 4000
 
-const url = 'mongodb://localhost/express'
+const  url= 'mongodb://127.0.0.1/express'
 
-mongoose.connect(url, {useNewUrlParser: true})
-const con = mongoose.connection
+mongoose.connect(url,{useNewUrlParser:true})
+const con =mongoose.connection
 
-con.on("open", () => {
-    console.log('MongoDB connected')
+con.on("open",()=>{
+    console.log('mongodb connected')
 })
+app.use(express.json());   //if we use json object inside our app.js
 
-app.use(express.json());
-//app.use('/customer',customer)
-//app.use('/item',item)
-//app.use('/user',user)
+// app.use('/item',item)
+// app.use('/users',user)
 
-
-
-// app.get('/', (req, res) => {
-//     console.log('Get Request has come')
-//     res.send('Hello World!')
-// })
-
-// app.get('/customer', (req, res) => {
-//     // console.log('Get Request has come')
-//     console.log(req.body)
-//     res.send('<h1>Customer get request has come !</h1>')
-// })
-//
-// app.get('/customer/:id', (req, res) => {
-//     // console.log('Get Request has come')
-//     console.log(req.keyParams);
-// })
-//
-// app.get('/customer', (req, res) => {
-//     // console.log('Get Request has come')
-//     console.log(req.params.id);
-// })
-//
-// app.post('/', (req,res) => {
-//     res.send('<h2>post request come for / route</h2>')
-// })
-//
-// app.post('/customer', (req,res) => {
-//     res.send('<h2>post request come for customer / route</h2>')
-//     console.log(req.body.id);
-// })
-//
-// app.delete('/customer', (req,res) => {
-//     console.log(req.query.id)
-//     res.send('<h2>delete request come for customer/ route</h2>')
-// })
-
- app.listen(port,(req,res) => {
-     console.log(`Example app listening on port ${port}`);
- })
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
